7a5b63281494a825ed6b88b343ebbc16e968ca5f
Emmanuel <epivasalazar@gmail.com>
2024-11-02 23:03:32 -0600

    feat:ModalErrorFuncional

--- src/components/organism/twobottonelection/TwoBottonElection.tsx
+++ src/components/organism/twobottonelection/TwoBottonElection.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button"
-import { useFormDepartament } from "@/hooks/Location/Departament"
+
 import { useFormMunicipality } from "@/hooks/Location/Municipality"
 import { useNavigate } from "react-router-dom";
 
 
 export const TwoBottonElection = () => {
-  const { handleClick: handleDepartamentClick } = useFormDepartament();
+  
   const { handleClick: handleMunicipalityClick } = useFormMunicipality();
   const navigation = useNavigate();
 
--- src/components/template/location/departament/ListDepartament/ListDepartament.tsx
+++ src/components/template/location/departament/ListDepartament/ListDepartament.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { ModalDelete, TableData } from "@/components/atom";
 import { Departament } from "@/interface";
 import { TableSkeleton } from "@/components/molecule";
